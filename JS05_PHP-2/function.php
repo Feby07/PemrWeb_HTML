7bc8cc85930fedb28c6af094575260f2ff07431b
Feby07 <rahmawatifeby5@gmail.com>
2025-09-28 22:41:01 +0700

    commit js5

--- JS05_PHP-2/function.php
+++ JS05_PHP-2/function.php
@@ -1,10 +1,16 @@
 <?php
-function perkenalan(){
-    echo "Assalamualaikum, <br/>";
-    echo "Perkenalkan, nama saya Elok<br/>";
+function perkenalan($nama, $salam){
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
     echo "Senang berkenalan dengan Anda<br/>";
 }
 
-perkenalan();
-perkenalan();
+perkenalan("Handana", "Hallo");
+
+echo "<hr>";
+
+$saya = "Elok";
+$ucapanSalam = "Selamat pagi";
+
+perkenalan($saya, $ucapanSalam);
 ?>
