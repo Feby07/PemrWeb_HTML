ee2560d5650d001406552e2dd08b15ef9a30fe48
Feby07 <rahmawatifeby5@gmail.com>
2025-09-28 22:38:21 +0700

    commit js5

---
+++ JS05_PHP-2/function.php
@@ -0,0 +1,10 @@
+<?php
+function perkenalan(){
+    echo "Assalamualaikum, <br/>";
+    echo "Perkenalkan, nama saya Elok<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+perkenalan();
+perkenalan();
+?>
