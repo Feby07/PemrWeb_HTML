4b9ebad2de57a6b85793ffb8cc8406c2ed65b313
Feby07 <rahmawatifeby5@gmail.com>
2025-10-13 01:59:46 +0700

    commit

--- formValidasiAjax.php
+++ formValidasiAjax.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Form Input dengan Validasi dan AJAX</title>
+    <title>Form Input dengan Validasi Password</title>
     <script src="jquery-3.7.1.js"></script>
 </head>
 <body>
-    <h3>Form Input dengan Validasi dan AJAX</h3>
-    <form id="myForm" method="post" action="proses_validasi_ajax.php">
+    <h3>Form Input dengan Validasi Password</h3>
+    <form id="myForm" method="post" action="proses_validasi_password.php">
         <label for="nama">Nama:</label>
         <input type="text" id="nama" name="nama">
         <span id="nama-error" style="color: red;"></span><br><br>
@@ -14,6 +14,10 @@
         <label for="email">Email:</label>
         <input type="text" id="email" name="email">
         <span id="email-error" style="color: red;"></span><br><br>
+
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password">
+        <span id="password-error" style="color: red;"></span><br><br>
 
         <input type="submit" value="Submit">
     </form>
@@ -26,6 +30,7 @@
                 event.preventDefault();
                 var nama = $("#nama").val();
                 var email = $("#email").val();
+                var password = $("#password").val();
                 var valid = true;
 
                 if (nama === "") {
@@ -42,9 +47,16 @@
                     $("#email-error").text("");
                 }
 
+                if (password.length < 8) {
+                    $("#password-error").text("Password minimal 8 karakter.");
+                    valid = false;
+                } else {
+                    $("#password-error").text("");
+                }
+
                 if (valid) {
                     $.ajax({
-                        url: "proses_validasi_ajax.php",
+                        url: "proses_validasi_password.php",
                         type: "POST",
                         data: $("#myForm").serialize(),
                         success: function (response) {
